f38fbf2b61fa3c9bf531c7985aa3fb7a64a040e9
vib1336 <vib1336@gmail.com>
2020-09-17 20:36:33 +0300

    Fix edit component

--- src/app/furniture/furniture-edit/furniture-edit.component.ts
+++ src/app/furniture/furniture-edit/furniture-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FurnitureService } from 'src/app/furniture.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Furniture } from 'src/app/Models/furniture';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
@@ -16,25 +16,46 @@ export class FurnitureEditComponent implements OnInit {
 
   constructor(private furnitureService: FurnitureService, 
     private route: ActivatedRoute, 
-    private formBuilder: FormBuilder) { }
+    private formBuilder: FormBuilder,
+    private router: Router) 
+    {
+      this.form = this.formBuilder.group({
+        make: [''],
+        model: [''],
+        year: [''],
+        description: [''],
+        price: [''],
+        image: [''],
+        material: ['']
+      });
+    }
 
   ngOnInit() {
     let id = this.route.snapshot.params['_id'];
-    this.furnitureService.getFurniture(id).subscribe(data => this.furniture = data);
-    this.form = this.formBuilder.group({
-      make: ['', [Validators.required, Validators.minLength(4)]],
-      model: ['', [Validators.required, Validators.minLength(4)]],
-      year: ['', [Validators.required, Validators.min(1950), Validators.max(2050)]],
-      description: ['', [Validators.required, Validators.minLength(10)]],
-      price: ['', [Validators.required, Validators.min(1)]],
-      image: ['', Validators.required],
-      material: ['', Validators.nullValidator]
+    this.furnitureService.getFurniture(id).subscribe(data => {
+      this.furniture = data
+
+      this.form = this.formBuilder.group({
+        make: [this.furniture.make],
+        model: [this.furniture.model],
+        year: [this.furniture.year],
+        description: [this.furniture.description],
+        price: [this.furniture.description],
+        image: [this.furniture.image],
+        material: ['', Validators.nullValidator]
+      });
     });
+    
   }
 
   editItem(){
     let id = this.route.snapshot.params['_id'];
-    this.furnitureService.editFurniture(id, this.form.value).subscribe(data => console.log(data));
+    this.furnitureService.editFurniture(id, this.form.value).subscribe(_ => {
+      
+      this.router.navigate(['/furniture/details/' + id]);
+    });
+    
+    
   }
 
 }
